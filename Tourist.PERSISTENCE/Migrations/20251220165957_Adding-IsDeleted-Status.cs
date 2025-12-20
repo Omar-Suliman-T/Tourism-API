7ecacf8f4e7e06f013ea69d1bd3e33d6a214dcd7
Ahmed Tarek <ahmedtarek7580@gmail.com>
2025-12-20 20:56:13 +0200

    ISDeletedStatus

---
+++ Tourist.PERSISTENCE/Migrations/20251220165957_Adding-IsDeleted-Status.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tourist.PERSISTENCE.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingIsDeletedStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Trips",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Reviews",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Places",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Payments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Norifications",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Hotels",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Countries",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Cities",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Categories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Activities",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Trips");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Places");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Norifications");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Hotels");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Countries");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Cities");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Activities");
+        }
+    }
+}
