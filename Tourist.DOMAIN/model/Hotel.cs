9741ff4dc7e278aef8d77079d27d137d6aa3efd6
AHmeDNasR54 <ahmed17nasr54@gmail.com>
2025-12-25 07:43:02 +0200

    Add hotel coordinates and near me endpoint

--- Tourist.DOMAIN/model/Hotel.cs
+++ Tourist.DOMAIN/model/Hotel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,18 @@ namespace Tourist.DOMAIN.model
         public string? Phone { get; set; }
         public string? ImageUrl { get; set; }
 
+        // Geolocation
+        [Range(-90, 90, ErrorMessage = "Latitude must be between -90 and 90")]
+        public double Latitude { get; set; }
+
+        [Range(-180, 180, ErrorMessage = "Longitude must be between -180 and 180")]
+        public double Longitude { get; set; }
+
         [ForeignKey("City")]
         public int CityId { get; set; }
         public City? City { get; set; }
+
+
         public IEnumerable<Trip>? Trips { get; set; }
     }
 }
